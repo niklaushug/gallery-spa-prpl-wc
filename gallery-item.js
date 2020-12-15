8c4aa0a4acf8b3b627ee5a961d80a0b2efd00ee9
Niklaus Hug <niklaus.hug@inventage.com>
2020-12-15 20:23:39 +0100

    Define content in gallery list and pass it to gallery item.

--- gallery-item.js
+++ gallery-item.js
@@ -1,11 +1,28 @@
 import { LitElement, html } from 'lit-element';
 
 class GalleryItem extends LitElement {
+  constructor() {
+    super();
+    this.artist = 'Magic penguin';
+    this.artistLink = '@magicpenguin';
+    this.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
+    this.url = 'https://via.placeholder.com/300';
+  }
+
+  static get properties() {
+    return {
+      artist: { type: String },
+      artistLink: { type: String },
+      description: { type: String },
+      url: { type: String },
+    };
+  }
+
   render() {
     return html`
       <figure class="gallery-item">
-        <img src="https://via.placeholder.com/150" alt="Dolor sit amet">
-        <figcaption>Dolor sit amet</figcaption>      
+        <img src="${this.url}" alt="picture of ${this.artist} on unsplash.com">
+        <figcaption>${this.description}</figcaption>      
       </figure>
     `;
   }
--- gallery-list.js
+++ gallery-list.js
@@ -1,17 +1,53 @@
 import { LitElement, html } from 'lit-element';
 
 class GalleryList extends LitElement {
+  constructor() {
+    super();
+    this.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
+    this.name = 'Lorem ipsum ';
+    this.images = [{
+        artist: 'Victoria Palacios',
+        artistLink: '@toriamia',
+        description: 'Glory and wonder here on little old earth. This shot is from a particularly magical hike through the Sequoia National Forest. We were so up high in the mountains that the clouds decided to settle around us and create a mystical fog all around us.',
+        url: 'https://images.unsplash.com/photo-1508788397430-55907b348ba8',
+      }, {
+        artist: 'Andrew Neel',
+        artistLink: '@andrewtneel',
+        description: '',
+        url: 'https://images.unsplash.com/photo-1477768663691-75454fd8e870',
+      }, {
+        artist: 'Casey Horner',
+        artistLink: '@mischievous_penguins',
+        description: 'Morning mystic',
+        url: 'https://images.unsplash.com/photo-1514509353532-1419c0935d72',
+      }]
+  }
+
+  static get properties() {
+    return {
+      name: { type: String },
+      description: { type: String },
+      images: { type: Array }
+    };
+  }
+
   render() {
     return html`
-      <div class="gallery-list">
-          <h2>Gallery - Lorem ipsum</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <ul class="layout">
-            <li>dummy</li>
-            <li>dummy</li>
-            <li>dummy</li>          
-          </ul>      
-      </div>    `;
+      <div class="gallery-list">          
+        <h2>${this.name}</h2>
+        <p>${this.description}</p>
+        <div class="layout">               
+          ${this.images.map((item) => html`
+            <gallery-item
+              artist="${item.artist}"
+              artistLink="${item.artistLink}"
+              description="${item.description}"
+              url="${item.url}"
+            ></gallery-item>                       
+          `)}              
+        </div>      
+      </div>
+    `;
   }
 }
 
